b4fb2dfcb2143fa1bfef7333893898416a251566
L3D <l3d@see-base.de>
2016-12-23 21:55:53 +0000

    Nothing happened

--- sendWithSerialConsole.py
+++ sendWithSerialConsole.py
@@ -9,8 +9,27 @@
 #
 import serial
 from random import randint
+from sys import argv
 
-ser = serial.Serial(
+udp = False
+serError = False #Falls es ein error beim seriell gibt
+for i in argv:
+    for i in argv:
+        if i in ["-h", "--help"]:
+            print("Dieses kleine Script soll die Kommunikation zwischen den WANDlern und dem PythonSpiel simulieren\n")
+            print("Standardmaeßig versucht das Spiel ueber das Device '/dev/ttyUSB0' die Serielle Schnitstelle zu oeffnen\n")
+            print("\t--udp\t- fuer Simulation ueber UDP")
+            print("\nWeitere Informationen auf github.com/see-base/schwebedraht und im SourceCode")
+            exit()
+        if i in ["-upd", "--udp"]:
+            udp = True
+            print("Verwende UDP")
+
+data="ID:0"+str(randint(0,4))+";INT:0"+str(randint(0,6))+";BAT:"+str(randint(10,99))+";F:"+str(randint(0,4))+"00;\n"
+ser = False
+if not udp:
+    try:
+        ser = serial.Serial(
             port='/dev/ttyUSB0',
                 baudrate=115200,
                     parity=serial.PARITY_NONE,
@@ -20,22 +39,25 @@ ser = serial.Serial(
                                     rtscts=False,
                                         dsrdtr=False
                                         )
+    except:
+        print("Moeglicherweise kein serielles Device angeschlossen")
+        serError = True
 
-try:
-    ser.open()
-except:
-    print("Serial Port already open")
-ser.isOpen()
+    if not serError:
+        try:
+            ser.open()
+        except:
+            print("Serial Port already open")
+        ser.isOpen()
+        print("Initializing the device ..")
+        ser.write(bytes(0x00))
+        print("Write bytes:")
+        ser.write(bytes(data, "UTF-8"))
+        print(bytes(data, "UTF-8"))
+        #   ser.write (bytes("ID:42;INT:11;BAT:79;F:100;\n", "UTF-8"))
+        #   print(bytes("ID:42;INT:11;BAT:79;F:100;\n", "UTF-8"))
+        print('Done')
 
-print("Initializing the device ..")
+if udp:
+    print("Noch nicht implementiert")
 
-ser.write(bytes(0x00))
-
-print("Write bytes:")
-data="ID:0"+str(randint(0,4))+";INT:0"+str(randint(0,6))+";BAT:"+str(randint(10,99))+";F:"+str(randint(0,4))+"00;\n"
-ser.write(bytes(data, "UTF-8"))
-print(bytes(data, "UTF-8"))
-#ser.write (bytes("ID:42;INT:11;BAT:79;F:100;\n", "UTF-8"))
-#print(bytes("ID:42;INT:11;BAT:79;F:100;\n", "UTF-8"))
-
-print('Done')
