fda7efe1bc9a4f33fe3cb11c1d3d5dcfecb42bb3
L3D - Lilian Roller <l3d@see-base.de>
2016-12-26 18:32:37 +0100

    bla, foo

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ try:
 except:
     pass
 
-spieler_liste = [(42, "green"), (23, "red"), (1337, "blue"), (0, "yellow")]
+spieler_liste = [(42, "green"), (23, "red"), (66, "blue"), (88, "yellow")]
 
 def main():
     global ID, INT, BAT, F
