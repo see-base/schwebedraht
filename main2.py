0051d134caf80fd65e4230d741f01869763cea92
L3D <l3d@see-base.de>
2016-12-26 15:48:59 +0000

    Vorbereiten Serial

--- main2.py
+++ main2.py
@@ -6,8 +6,7 @@
 
 from pygame import mixer
 import socket
-import os
-
+import random, serial
 
 vfx = { "fail" : ["Pesthoernchen.png"],
                     "start" : [],
@@ -19,6 +18,10 @@ sfx = [] # 0 = fail, 1 = start, 2 = bonus, 3 = ende
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.connect(("127.0.0.1", 4444))
 
+#init serial
+
+ser = serial.Serial("/dev/ttyUSB0", 115200)
+
 #init audio-system
 try:
 	mixer.init()
@@ -29,6 +32,7 @@ try:
 
 except:
     pass
+
 def main():
     while True:
         ID, INT, BAT, F = serial_decoder()
@@ -45,8 +49,8 @@ def main():
             aktion(0)
 
 def serial_decoder():
-    input_string = input() #ser.readline()
-    input_split = input_string.split(";")
+    input_string = ser.readline()
+    input_split = input_string.decode().split(";")
 
     return (int(input_split[0][3:]),
         int(input_split[1][4:]),
@@ -76,36 +80,23 @@ def effekt(segment):
 def visueller_effekt(vfx_index):
 	if vfx_index == 0:
 		sock.send(bytes("medien/zoom_exponential:" + str(0), "UTF-8"))
-		sock.send(bytes("medien/effekt/bildname:" + str("Pesthoernchen.png"), "UTF-8"))
+		sock.send(bytes("medien/effekt/bildname:" + random.choice(vfx["fail"]), "UTF-8"))
 		sock.send(bytes("medien/zoom:" + str(1), "UTF-8"))
 	elif vfx_index == 1:
 		sock.send(bytes("medien/ausw:" + str("0"), "UTF-8")) # ausblenden auswertung-node
-		sock.send(bytes("medien/punkte/punkte:{} | {}".format(punkte, p_multiplikator), "UTF-8"))
+		sock.send(bytes("medien/punkte/punkte:{}".format("00:00"), "UTF-8")) #spaeter: aktuelle zeit
 		sock.send(bytes("medien/hintergrund/alpha:" + str("1"), "UTF-8"))
 	elif vfx_index == 2:
 		sock.send(bytes("medien/zoom_exponential:" + str(1), "UTF-8"))
-		sock.send(bytes("medien/effekt/bildname:" + str("star.png"), "UTF-8"))
+		sock.send(bytes("medien/effekt/bildname:" + random.choice(vfx["ziel"]), "UTF-8"))
 		sock.send(bytes("medien/zoom:" + str(1), "UTF-8"))
 	elif vfx_index == 3:
 		sock.send(bytes("medien/punkte/punkte:" + str("Auswertung:"), "UTF-8"))
 		sock.send(bytes("medien/hintergrund/alpha:" + str("0"), "UTF-8")) #ausblenden "See-Base"
 		sock.send(bytes("medien/ausw:" + str("1"), "UTF-8")) #einblenden auswertung-node
 
-		#sock.send(bytes("medien/auswertung/punkte:" + str(punkte), "UTF-8")) # Gesamtpunkte
-		sock.send(bytes("medien/auswertung/zeit:" + str("%.4f" %(zeit - startzeit)), "UTF-8")) # Gesamtzeit
-		sock.send(bytes("medien/auswertung/nick:" + str(nick), "UTF-8")) # Unique Nickname
-
-		sock.send(bytes("medien/auswertung/segment1a:" + str("Zeit: %.2f"%(seg1[2])), "UTF-8")) # 1. Segment - Zeit
-		sock.send(bytes("medien/auswertung/segment1b:" + str(str(seg1[3]) +" x Berührt"), "UTF-8"))  # 1. Segment - Beruehrungen
-		sock.send(bytes("medien/auswertung/segment2a:" + str("Zeit: %.2f"%(seg2[2])), "UTF-8")) # 2. Segment - Zeit
-		sock.send(bytes("medien/auswertung/segment2b:" + str(str(seg2[3]) +" x Berührt"), "UTF-8"))  #2. Segment - Beruehrungen
-		sock.send(bytes("medien/auswertung/segment3a:" + str("Zeit: %.2f"%(seg3[2])), "UTF-8")) # 3. Segment - Zeit
-		sock.send(bytes("medien/auswertung/segment3b:" + str(str(seg3[3]) +" x Berührt"), "UTF-8"))  #3. Segment - Beruehrungen
-		sock.send(bytes("medien/auswertung/segment4a:" + str("Zeit: %.2f"%(seg4[2])), "UTF-8")) # 4. Segment - Zeit
-		sock.send(bytes("medien/auswertung/segment4b:" + str(str(seg4[3]) +" x Berührt"), "UTF-8"))  #4. Segment - Beruehrungen
-		sock.send(bytes("medien/auswertung/segment5a:" + str("Zeit: %.2f"%(seg5[2])), "UTF-8")) # 5. Segment - Zeit
-		sock.send(bytes("medien/auswertung/segment5b:" + str(str(seg5[3]) +" x Berührt"), "UTF-8"))  #5. Segment - Beruehrungen
-
 def audio_effekt(sfx_index):
-	if len(sfx > 0):
+	if len(sfx) > 0:
 		mixer.Sound.play(sfx[sfx_index])
+
+main()
