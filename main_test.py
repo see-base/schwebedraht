d98f53bc465209b3e3e8463932cb9a064f9a5937
Talvi <talvi@see-base.de>
2018-06-01 21:25:58 +0200

    Zeitnahme und Effekt-Logik überarbeitet

--- main_test.py
+++ main_test.py
@@ -3,18 +3,40 @@
 
 # Schwebedraht - Ein Spiel der see-base
 
-import RPi.GPIO as GPIO # Raspi GPIO-Bibliothek
-from pygame import mixer
-from time import sleep
+# --- Bibliotheken importieren --- #
 
-# Helferfunktionen
+import RPi.GPIO as GPIO
+from time import time, sleep
 
-# effektdaten zum info-beamer via socket schicken
-def ib_send(sock, data):
+
+# --- Globale Variablen --- #
+
+sock = None
+
+segmente = {
+    "start": [37]
+    "bonus": [33, 29, 18]
+    "malus": [35, 31, 22, 16]
+    "stopp": [12]
+}
+
+startzeit = 0.0
+zeiten_liste = []
+
+punkte = 0
+p_faktor = 1
+
+running = False
+
+
+# --- Helferfunktionen --- #
+
+# Effektdaten zum info-beamer via socket schicken
+def ib_send(data):
     sock.send(bytes(data, "UTF-8"))
 
 
-# Initialisierung
+# --- Initialisierung --- #
 
 def init_gpio():
     GPIO.setwarnings(False)
@@ -26,51 +48,91 @@ def init_gpio():
     print("GPIOs eingerichtet")
 
 def init_socket():
+	global sock
+
     sock = socket.socket(socket.AF_INET, socket_DGRAM)
     sock.connect(("127.0.0.1", "4444"))
 
-    ib_send(sock, "medien/connected:True")
-    ib_send(sock, "medien/punkte/punkte:#see-base")
+    ib_send("medien/connected:True")
+    ib_send("medien/punkte/punkte:#see-base")
 
     print("Info-Beamer Verbindung hergestellt")
 
 
+# --- Hauptschleife --- #
+
 def main():
-    # Spiel-Variablen einrichten
-    segmente = {
-        "start": [37]
-        "bonus": [33, 29, 18]
-        "malus": [35, 31, 22, 16]
-        "stopp": [12]
-    }
-
-    running = False
-
+	global running, zeiten_liste
     # Spiel initialisieren
     init_gpio()
     init_socket()
 
     # Spiel starten
     while True:
-        for key, value in segmente:
+    
+        for key, value in segmente.items():
+        
             for pin in value:
+            
                 if not GPIO.input(pin):
+                
                     if running:
-                        if key == "malus":
-                            pass
-                        elif key == "bonus":
-                            pass
-                        elif key == "stopp":
-                            pass
+                    	# Zeitstempel machen
+                    	get_time(key, pin)
+                    	
+            			# Effekt spielen
+                        effekt(key)
+                        
                         sleep(0.2)
-
                     else:
+                    
                         if key == "start":
+                        	get_time(key, pin)
                             running = True
 
 
+# Speichert die Pin-ID und die Zeit seit dem Spielstart in zeiten_liste
+def get_time(name, pin):
+    global startzeit, zeiten_liste
+    
+    zeit = time()
 
+    if name == "start":
+        startzeit = zeit
+    
+    # Zeitabstände unter einer Sekunde werden ignoriert
+    if name == "start" or zeit - zeiten_liste[-1][1] > 1:
+		zeiten_liste.append((pin, zeit - startzeit))
+	
+	print("Zeitstempel für Pin {} gesetzt: {}", pin, zeiten_liste[-1][1])
 
+# Sendet Daten an den Info-Beamer um dort einen Effekt zu spielen.
+# TODO: SFX einbauen.
+def effekt(name):
+	
+	if name == "start":
+		print("Spiele Start-Effekt"...)
+		
+		ib_send("medien/ausw:0")
+		ib_send("medien/punkte/punkte:{} | {}".format(punkte, p_multiplikator))
+		ib_send("medien/hintergrund/alpha:1")
 
+	elif name == "malus":
+		print("Spiele Malus-Effekt...")
+		
+		ib_send("medien/zoom_exponential:0")
+		ib_send("medien/effekt/bildname:pesthoernchen.png")
+		ib_send("medien/zoom:1)
+
+	elif name == "bonus":
+		print("Spiele Bonus-Effekt...")
+		
+		ib_send("medien/zoom_exponential:1")
+		ib_send("medien/effekt/bildname:star.png")
+		ib_send("medien/zoom:1")
+
+	elif name == "stopp":
+		print("Spiele Stopp-Effekt...")
+		#TODO Endanimation erstellen
 
 
